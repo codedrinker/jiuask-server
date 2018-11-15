86d41a75306fe2dd93f0f4465d92049e09f4ac0e
codedrinker <gwangchunlei@gmail.com>
2018-11-16 06:42:43 +0800

    修改名称

--- src/main/java/com/codedrinker/Application.java
+++ src/main/java/com/codedrinker/Application.java
@@ -4,7 +4,6 @@ package com.codedrinker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @SpringBootApplication
 @ComponentScan
-@Configuration
 @ImportResource({"classpath*:applicationContext.xml"})
 public class Application {
 
