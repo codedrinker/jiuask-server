7bb15d6133019074a36c1d56a6c20a46930c20cd
codedrinker <gwangchunlei@gmail.com>
2018-11-26 07:05:13 +0800

    添加更多注释

--- src/main/java/com/codedrinker/controller/LoginController.java
+++ src/main/java/com/codedrinker/controller/LoginController.java
@@ -18,22 +18,31 @@ import java.util.UUID;
 
 /**
  * Created by codedrinker on 2018/11/24.
+ * RestController 定义 RESTFUL Controller，默认返回 JSON 数据
+ * Slf4j 添加 LOG 注解，自动注入 log以便打印日志
  */
 @RestController
 @Slf4j
 public class LoginController {
 
+    // Spring 自动注入 wechatAdapter，因 WechatAdapter 类上面有 @Service 注解
     @Autowired
     private WechatAdapter wechatAdapter;
 
+    // 定义 domain/api/login 访问接口，用于实现登录
+    // 使用 LoginDTO 自动解析传递过来的 JSON 数据
     @RequestMapping("/api/login")
     public ResultDTO login(@RequestBody LoginDTO loginDTO) {
         try {
             log.info("login request : {}", loginDTO);
+            // 使用 code 调用微信 API 获取 session_key 和 openid
             SessionDTO sessionDTO = wechatAdapter.jscode2session(loginDTO.getCode());
             log.info("login get session : {}", sessionDTO);
+
+            // 检验传递过来的使用户信息是否合法
             DigestUtil.checkDigest(loginDTO.getRawData(), sessionDTO.getSessionKey(), loginDTO.getSignature());
             //TODO: 储存 token
+            //生成token，用于自定义登录态，这里的存储逻辑比较复杂，放到下一讲
             TokenDTO data = new TokenDTO();
             data.setToken(UUID.randomUUID().toString());
             return ResultDTO.ok(data);
--- src/main/java/com/codedrinker/util/DigestUtil.java
+++ src/main/java/com/codedrinker/util/DigestUtil.java
@@ -2,22 +2,20 @@ package com.codedrinker.util;
 
 import com.codedrinker.error.CommonErrorCode;
 import com.codedrinker.error.ErrorCodeException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
-
-import java.io.UnsupportedEncodingException;
 
 /**
  * Created by codedrinker on 2018/11/25.
  */
+@Slf4j
 public class DigestUtil {
     public static void checkDigest(String rawData, String sessionKey, String signature) {
-        try {
-            String sha1 = DigestUtils.sha1Hex((rawData + sessionKey).getBytes("UTF-8"));
-            boolean equals = sha1.equals(signature);
-            if (!equals) {
-                throw new ErrorCodeException(CommonErrorCode.SIGNATURE_ERROR);
-            }
-        } catch (UnsupportedEncodingException e) {
+        log.info("rawData:{},sessionKey:{},signature:{}", rawData, sessionKey, signature);
+        // 调用 apache 的公共包进行 SHA1 加密处理
+        String sha1 = DigestUtils.sha1Hex((rawData + sessionKey).getBytes());
+        boolean equals = sha1.equals(signature);
+        if (!equals) {
             throw new ErrorCodeException(CommonErrorCode.SIGNATURE_ERROR);
         }
     }
